2c712c18733951882c80b02dd002e73203127b3c
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2024-10-04 22:23:48 +0530

    learning storybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	staticDirs: ['../public'],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@chromatic-com/storybook',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
+};
+export default config;
